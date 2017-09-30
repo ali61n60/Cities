ccdabce285ea2e16e07e3c0161d10fce58f91ac3
Ali <ali62n62@yahoo.com>
2017-09-30 23:56:47 +0330

    ok

--- Cities/Infrastructure/TagHelpers/CoordinatingTagHelpers.cs
+++ Cities/Infrastructure/TagHelpers/CoordinatingTagHelpers.cs
@@ -10,23 +10,21 @@ namespace Cities.Infrastructure.TagHelpers
     public class ButtonGroupThemeTagHelper : TagHelper
     {
         public string Theme { get; set; }
-        public override void Process(TagHelperContext context,
-            TagHelperOutput output)
+        public override void Process(TagHelperContext context,TagHelperOutput output)
         {
             context.Items["theme"] = Theme;
         }
     }
+
     [HtmlTargetElement("button", ParentTag = "div")]
     [HtmlTargetElement("a", ParentTag = "div")]
     public class ButtonThemeTagHelper : TagHelper
     {
-        public override void Process(TagHelperContext context,
-            TagHelperOutput output)
+        public override void Process(TagHelperContext context,TagHelperOutput output)
         {
             if (context.Items.ContainsKey("theme"))
             {
-                output.Attributes.SetAttribute("class",
-                    $"btn btn-{context.Items["theme"]}");
+                output.Attributes.SetAttribute("class",$"btn btn-{context.Items["theme"]}");
             }
         }
     }
